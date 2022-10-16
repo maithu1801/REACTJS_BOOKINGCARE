35e6e6f9c3dc17981bc62c96f7e7373e8346c7c7
Mai Thu <maithu1801tv@gamil.com>
2022-10-16 13:00:40 +0700

    reactjs v42

---
+++ src/services/userService.js
@@ -0,0 +1,27 @@
+import axios from '../axios';
+
+const handleLoginApi = (userEmail, userPassword) => {
+    return axios.post('/api/login', { email: userEmail, password: userPassword });
+}
+const getAllUsers = (inputId) => {
+    //template string
+    return axios.get(`/api/get-all-users?id=${inputId}`)
+}
+
+const createNewUserService = (data) => {
+    console.log('check data from service: ', data)
+    return axios.post('/api/create-new-user', data)
+}
+
+const deleteUserService = (userId) => {
+    return axios.delete('/api/delete-user', {
+        data: {
+            id: userId
+        }
+    });
+}
+
+
+
+
+export { handleLoginApi, getAllUsers, createNewUserService, deleteUserService }
