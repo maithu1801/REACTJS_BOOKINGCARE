40e04a84347137e3966c8863ac08ee7ce246c4c2
Mai Thu <maithu1801tv@gamil.com>
2022-11-01 10:57:01 +0700

    reactjs v76

--- src/containers/Patient/Doctor/DoctorSchedule.js
+++ src/containers/Patient/Doctor/DoctorSchedule.js
@@ -13,6 +13,7 @@ class DoctorSchedule extends Component {
         super(props);
         this.state = {
             allDays: [],
+            allAvalableTime: []
         }
     }
 
@@ -24,13 +25,20 @@ class DoctorSchedule extends Component {
         this.setArrDays(language);
     }
 
+    // viet in hoa chu cai dau tien
+    capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+
     setArrDays = (language) => {
         let allDays = []
 
         for (let i = 0; i < 7; i++) {
             let object = {};
             if (language === LANGUAGES.VI) {
-                object.label = moment(new Date()).add(i, 'days').format('dddd - DD/MM');
+                let labelVi = moment(new Date()).add(i, 'days').format('dddd - DD/MM');
+                object.label = this.capitalizeFirstLetter(labelVi)
             } else {
                 object.label = moment(new Date()).add(i, 'days').locale('en').format('ddd - DD/MM');
             }
@@ -56,12 +64,18 @@ class DoctorSchedule extends Component {
             let date = event.target.value
             let res = await getScheduleDoctorByDate(doctorId, date);
 
-            console.log('check res schedule from react: ', res)
+            if (res && res.errCode === 0) {
+                this.setState({
+                    allAvalableTime: res.data ? res.data : []
+                })
+            }
+            console.log('check res schedule from react: ', res);
         }
     }
 
     render() {
-        let { allDays } = this.state;
+        let { allDays, allAvalableTime } = this.state;
+        let { language } = this.props;
         return (
             <div className='doctor-schedule-container'>
                 <div className='all-schedule'>
@@ -77,7 +91,22 @@ class DoctorSchedule extends Component {
                     </select>
                 </div>
                 <div className='all-availabel-time'>
-
+                    <div className='text-calendar'>
+                        <i className='fas fa-calendar-alt'><span>Lịch khám</span></i>
+                    </div>
+                    <div className='time-content'>
+                        {allAvalableTime && allAvalableTime.length > 0 ?
+                            allAvalableTime.map((item, index) => {
+                                let timeDisplay = language === LANGUAGES.VI ?
+                                    item.timeTypeData.valueVi : item.timeTypeData.valueEn;
+                                return (
+                                    <button key={index}>{timeDisplay}</button>
+                                )
+                            })
+                            :
+                            <div> Không có lịch hẹn trong thời gian này, vui lòng chọn thời gian khác!</div>
+                        }
+                    </div>
                 </div>
             </div>
 
